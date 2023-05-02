1001f1bad444f9886fee1258dfe5520611d284da
acethecreator <devaze007@gmail.com>
2023-05-02 12:33:18 +0100

    added db connection to account service

--- AccountService/src/api/index.js
+++ AccountService/src/api/index.js
@@ -2,6 +2,7 @@ const Hermes = require("hermesjs");
 const app = new Hermes();
 const path = require("path");
 const { yellow, gray, cyan } = require("chalk");
+const mongoose = require("mongoose");
 const buffer2string = require("./middlewares/buffer2string");
 const string2json = require("./middlewares/string2json");
 const json2string = require("./middlewares/json2string");
@@ -13,12 +14,15 @@ const AmqpAdapter = require("hermesjs-amqp");
 const userSignup = require("./routes/user-signup.js");
 const userLogin = require("./routes/user-login.js");
 const userQueue = require("./routes/user-queue.js");
+const mongoDB = "mongodb://127.0.0.1/headless";
 
 app.addAdapter(AmqpAdapter, serverConfig);
 
 app.use(buffer2string);
 app.use(string2json);
 app.use(logger);
+
+mongoose.set("strictQuery", false);
 
 // Channels
 console.log(
@@ -48,6 +52,15 @@ app.useOutbound(json2string);
 app
   .listen()
   .then((adapters) => {
+    mongoose.connect(mongoDB).then(() => {
+console.log(
+  "Database connection to",
+  cyan.underline(`${mongoDB}`),
+  "is ready"
+);
+    }).catch((err) => {
+      console.log(err)
+    })
     console.log(
       cyan.underline(`${config.app.name} ${config.app.version}`),
       gray("is ready!"),
